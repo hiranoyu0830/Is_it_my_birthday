21266c50c32fb77bd541395aac51c3901cfafb96
hiranoyu0830 <c197034@ed.nagoya-cu.ac.jp>
2022-01-15 11:01:16 +0900

    first commit

---
+++ lecture3/local_secret.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'django-insecure-)2i2!*hk+tr#0#67vap4l-4vdv(xw(l9%!vby!bi=xj7_+5fiw'
--- lecture3/settings.py
+++ lecture3/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from .local_secret import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)2i2!*hk+tr#0#67vap4l-4vdv(xw(l9%!vby!bi=xj7_+5fiw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
